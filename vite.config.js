a905ce43d16c69bdcdb6879473e54de1b997b855
daffaa22 <muhammaddaffa10244@gmail.com>
2026-09-09 20:37:52 +0700

    feat: expose Vite dev server to local network (LAN / Wi-Fi)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Membuka akses untuk semua perangkat di satu jaringan (LAN / Wi-Fi)
+    port: 5173,
+    cors: true
+  }
 })
